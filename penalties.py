e85a258e771f2b587f528e1f72332ba8f9f88b67
jblanca2 <124798747+jblanca2@users.noreply.github.com>
2023-03-22 09:15:11 -0400

    Update penalties.py

--- penalties.py
+++ penalties.py
@@ -10,8 +10,12 @@ class Penalty:
 		if keyboard.is_pressed("esc"):
 			print("The total score is: " , score)
 			break
-		elif keyboard.is_pressed("space"):
+		elif keyboard.is_pressed("1"):
 			penalty += 1
-
+		elif keyboard.is_pressed("2"):
+			penalty += 2
+		elif keyboard.is_pressed("space"):
+			penalty += 0.5
+			
 	def getScore(self):
 		return penalty
