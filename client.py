2fd3bc499d2106df86b9a53e3d41a15beafd96d7
Dareen Abdullah <115726578+DareenAbdullah@users.noreply.github.com>
2023-04-27 15:28:41 -0400

    Update client.py

--- client.py
+++ client.py
@@ -12,25 +12,14 @@ client_socket.connect((host_ip, port))
 data =b""
 payload_size = struct.calcsize("Q")
 while True:
-    while len(data) < payload_size:
-        packet = client_socket.recv(4*1024) #4k
-        if not packet:
-            break
-        data += packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q", packed_msg_size)[0]
+    vid = cv2.VideoCapture(0)
+    while (vid.isOpened()):
+        img,frame = vid.read()
+        a = pickle.dumps(frame)
+        message = struct.pack("Q",len(a)) + a
+        client_socket.sendall(message)
+        cv2.imshow('TRANSMITTING VIDEO', frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            client_socket.close()
 
-    while len(data) < msg_size:
-        data += client_socket.recv(4*1024)
-
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    frame = pickle.loads(frame_data)
-    cv2.imshow("Received", frame)
-    key = cv2.waitKey(1) & 0xFF
-    
-    if key == ord('q'):
-        break
-
-client_socket.close()
